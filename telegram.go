26a36491a74e36f2e755e5ff296398be02b6d8d5
faramin esmaili seraji <faramin.smile@gmail.com>
2017-04-01 20:04:02 +0430

    Telegram logger should not make program sleep.

--- telegram.go
+++ telegram.go
@@ -7,44 +7,42 @@ import (
 )
 
 type telegSteam struct {
-	bot     *telegram.BotAPI
-	buf     []byte
-	maxSize int
-	chatID  int64
-	mutex   sync.Mutex
+	bot    *telegram.BotAPI
+	chatID int64
+	mutex  sync.Mutex
+	queue  chan string
+	cancel chan struct{}
 }
 
-func newTelegSteam(token string, chatID int64, maxSize int) (*telegSteam, error) {
+func newTelegSteam(token string, chatID int64, chanSize int) (*telegSteam, error) {
 	bot, err := telegram.NewBotAPI(token)
-	return &telegSteam{bot: bot, buf: make([]byte, 0), maxSize: maxSize, chatID: chatID, mutex: sync.Mutex{}}, err
+	tele := &telegSteam{bot: bot, queue: make(chan string, chanSize), chatID: chatID, mutex: sync.Mutex{}, cancel: make(chan struct{})}
+	go tele.flusher()
+	return tele, err
 }
 
 func (t *telegSteam) Write(buf []byte) (n int, err error) {
 
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
+	t.queue <- string(buf)
 
-	for i := 0; n < len(buf); i++ {
-		min := t.maxSize
-		if min > len(buf) {
-			min = len(buf) - n
-		}
-
-		chunk := buf[i*t.maxSize : min]
-		t.buf = append(t.buf, chunk...)
-		if er := t.flush(); er != nil {
-			err = er
-			return
-		}
-		n += len(chunk)
-
-	}
-	return
+	return len(buf), nil
 }
 
-func (t *telegSteam) flush() error {
-	msg := telegram.NewMessage(t.chatID, string(t.buf))
-	_, err := t.bot.Send(msg)
-	t.buf = t.buf[0:0]
-	return err
+func (t *telegSteam) Cancel() {
+	t.cancel <- struct{}{}
+	close(t.queue)
+}
+
+func (t *telegSteam) flusher() {
+	for {
+		select {
+		case <-t.cancel:
+			return
+		case item := <-t.queue:
+			msg := telegram.NewMessage(t.chatID, item)
+			t.bot.Send(msg)
+		}
+	}
 }
